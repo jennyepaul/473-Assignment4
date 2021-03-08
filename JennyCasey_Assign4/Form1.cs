f28238ca0342e37b223969b10e6ba9fb5a387c7a
Jenny Paul <jenny@LAPTOP-PQ2SB0U6>
2021-03-08 11:53:27 -0600

    adding in the drawing colors

--- JennyCasey_Assign4/Form1.cs
+++ JennyCasey_Assign4/Form1.cs
@@ -12,6 +12,15 @@ namespace JennyCasey_Assign4
 {
     public partial class Form1 : Form
     {
+        //the following are the 4 colors that we can use to draw
+        public static System.Drawing.Color Red { get; }
+        public static System.Drawing.Color Black { get; }
+        public static System.Drawing.Color Blue { get; }
+        public static System.Drawing.Color Green { get; }
+
+
+
+
         public Form1()
         {
             InitializeComponent();
